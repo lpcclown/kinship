edf96ec1dd41494726e8f349d1adbf9427f496a6
Peter Pinchao Liu <281538707@qq.com>
2017-06-14 00:40:36 -0400

    Add the function to upload image to php server
    Only works for first photo.

--- Detector/ViewController.swift
+++ Detector/ViewController.swift
@@ -8,6 +8,22 @@
 
 import UIKit
 import CoreImage
+import MobileCoreServices
+
+extension Data {
+    
+    /// Append string to NSMutableData
+    ///
+    /// Rather than littering my code with calls to `dataUsingEncoding` to convert strings to NSData, and then add that data to the NSMutableData, this wraps it in a nice convenient little extension to NSMutableData. This converts using UTF-8.
+    ///
+    /// - parameter string:       The string to be added to the `NSMutableData`.
+    
+    mutating func append(_ string: String) {
+        if let data = string.data(using: .utf8) {
+            append(data)
+        }
+    }
+}
 
 class ViewController: UIViewController,
     UIImagePickerControllerDelegate,
@@ -103,8 +119,115 @@ UINavigationControllerDelegate {
     }
     
     @IBAction func comparResult(_ sender: Any) {
+        
+        createRequest(userid: "abc", password: "password", email: "mail@cc.edu")
+        
+    }
+    /// Create request
+    ///
+    /// - parameter userid:   The userid to be passed to web service
+    /// - parameter password: The password to be passed to web service
+    /// - parameter email:    The email address to be passed to web service
+    ///
+    /// - returns:            The NSURLRequest that was created
+    
+    func createRequest(userid: String, password: String, email: String) {
+        let parameters = [
+            "user_id"  : userid,
+            "email"    : email,
+            "password" : password]  // build your dictionary however appropriate
+        
+        let boundary = generateBoundaryString()
+        
+        let url = URL(string: "http://104.55.84.205/index.php")!
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+        //let path1 = Bundle.main.path(forResource: "image1", ofType: "png")!
+        let imageData = UIImageJPEGRepresentation(firstImageView.image!, 1)
+        if (imageData == nil) {print("tttttttt")}
+        request.httpBody = createBody(with: parameters, filePathKey: "file", imageDataKey: imageData! as NSData, boundary: boundary) as Data
+        let task = URLSession.shared.dataTask(with: request){ data, response, error in
+            guard let data = data, error == nil else {
+                print("error=\(String(describing: error))")
+                return
+            }
+            print (response!)
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
+                print("statusCode should be 200, but is \(httpStatus.statusCode)")
+                print("response = \(String(describing: response))")
+            }
+            let responseString = String(data: data, encoding: .utf8)
+            print("responseString = \(String(describing: responseString))")
+        }
+        task.resume()
     }
     
+    /// Create body of the multipart/form-data request
+    ///
+    /// - parameter parameters:   The optional dictionary containing keys and values to be passed to web service
+    /// - parameter filePathKey:  The optional field name to be used when uploading files. If you supply paths, you must supply filePathKey, too.
+    /// - parameter paths:        The optional array of file paths of the files to be uploaded
+    /// - parameter boundary:     The multipart/form-data boundary
+    ///
+    /// - returns:                The NSData of the body of the request
+    
+    func createBody(with parameters: [String: String]?, filePathKey: String, imageDataKey: NSData, boundary: String) -> NSData {
+        var body = Data()
+        
+        if parameters != nil {
+            for (key, value) in parameters! {
+                body.append("--\(boundary)\r\n")
+                body.append("Content-Disposition: form-data; name=\"\(key)\"\r\n\r\n")
+                body.append("\(value)\r\n")
+            }
+        }
+        
+        
+        //let url = URL(fileURLWithPath: path)
+        let filename = "user-profile.JPG"
+        //let data = try Data(contentsOf: url)
+        let mimetype = "image/jpg"
+        
+        body.append("--\(boundary)\r\n")
+        body.append("Content-Disposition: form-data; name=\"\(filePathKey)\"; filename=\"\(filename)\"\r\n")
+        body.append("Content-Type: \(mimetype)\r\n\r\n")
+        //body.append(String(data:imageDataKey as Data, encoding: String.Encoding.utf8)!)
+        body.append(imageDataKey.base64EncodedData(options: NSData.Base64EncodingOptions(rawValue: 0)))
+        body.append("\r\n")
+        
+        body.append("--\(boundary)--\r\n")
+        print(body)
+        return body as NSData
+    }
+    
+    /// Create boundary string for multipart/form-data request
+    ///
+    /// - returns:            The boundary string that consists of "Boundary-" followed by a UUID string.
+    
+    func generateBoundaryString() -> String {
+        return "Boundary-\(NSUUID().uuidString)"
+    }
+    
+    /// Determine mime type on the basis of extension of a file.
+    ///
+    /// This requires MobileCoreServices framework.
+    ///
+    /// - parameter path:         The path of the file for which we are going to determine the mime type.
+    ///
+    /// - returns:                Returns the mime type if successful. Returns application/octet-stream if unable to determine mime type.
+    
+    func mimeType(for path: String) -> String {
+        let url = NSURL(fileURLWithPath: path)
+        let pathExtension = url.pathExtension
+        
+        if let uti = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, pathExtension! as NSString, nil)?.takeRetainedValue() {
+            if let mimetype = UTTypeCopyPreferredTagWithClass(uti, kUTTagClassMIMEType)?.takeRetainedValue() {
+                return mimetype as String
+            }
+        }
+        return "application/octet-stream";
+    }
     
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingImage image: UIImage!, editingInfo: NSArray!){
